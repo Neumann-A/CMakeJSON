d6094215fcaef6144201c2f2cd3e27e9a04482bb
Alexander Neumann <Alexander.Neumann@hamburg.de>
2021-02-24 20:11:39 +0100

    more target stuff

--- CMakeJSON/CMakeJSON_Target.cmake
+++ CMakeJSON/CMakeJSON_Target.cmake
@@ -10,6 +10,12 @@ function(cmakejson_gather_json_array_as_list _prefix _outvar)
     set(${_outvar} ${${_outvar}} PARENT_SCOPE)
 endfunction()
 
+function(cmakejson_gather_json_array_target_link_libraries_as_list _prefix _outvar)
+    set(element ${${_prefix}})
+    list(APPEND ${_outvar} ${element})
+    set(${_outvar} ${${_outvar}} PARENT_SCOPE)
+endfunction()
+
 function(cmakejson_target_redirection)
 endfunction()
 
@@ -24,11 +30,13 @@ function(cmakejson_add_target _input _filename)
                          OUTPUT_LIST_CREATED_VARIABLES "TARGET_PARSED_VARIABLES"
     )
     list(POP_BACK CMAKE_MESSAGE_CONTEXT)
-    cmakejson_message_if(CMakeJSON_DEBUG_TARGET STATUS "Variables created by target parse:")
-    cmakejson_print_variables_if(CMakeJSON_DEBUG_TARGET TARGET_PARSED_VARIABLES)
+    cmakejson_message_if(CMakeJSON_DEBUG_TARGET_VERBOSE "Variables created by target parse:")
+    cmakejson_print_variables_if(CMakeJSON_DEBUG_TARGET_VERBOSE TARGET_PARSED_VARIABLES)
 
     if(NOT DEFINED CMakeJSON_PARSE_TARGET_NAME)
         set(CMakeJSON_PARSE_TARGET_NAME "${_filename}")
+    else()
+        cmake_language(EVAL CODE "set(CMakeJSON_PARSE_TARGET_NAME ${CMakeJSON_PARSE_TARGET_NAME})") # Evaluate variables stored in parsed variables.
     endif()
     if(NOT CMakeJSON_PARSE_TARGET_TARGET_TYPE MATCHES "library|executable|custom_target")
         message(${CMakeJSON_MSG_ERROR_TYPE} "Unknown value in member 'target_type' in target ${CMakeJSON_PARSE_TARGET_NAME}. Allowed values !")
@@ -44,27 +52,29 @@ function(cmakejson_add_target _input _filename)
     cmake_language(CALL ${target_command} ${target_name} ${target_params} ${target_sources})
     cmakejson_message_if(CMakeJSON_DEBUG_TARGET "Add target: ${target_command}(${target_name} ${target_params} ${target_sources})")
 
-    set(target_command_list include_directories
+    set(target_command_list 
                             compile_definitions
-                            compile_options
                             compile_features
-                            link_libraries)
+                            compile_options
+                            include_directories
+                            link_directories
+                            link_libraries
+                            link_options
+                            precompile_headers)
     
     foreach(_command IN LISTS target_command_list)
         string(TOUPPER "${_command}" parse_command)
         set(_command target_${_command})
         foreach(_access PUBLIC PRIVATE INTERFACE)
             set(_params)
-            cmakejson_message_if(CMakeJSON_DEBUG_TARGET "Checking: CMakeJSON_PARSE_TARGET_${parse_command}_${_access}")
-            cmakejson_run_func_over_parsed_range(CMakeJSON_PARSE_TARGET_${parse_command}_${_access} cmakejson_gather_json_array_as_list _params)
+            cmakejson_message_if(CMakeJSON_DEBUG_TARGET_VERBOSE "Checking: CMakeJSON_PARSE_TARGET_${parse_command}_${_access}")
+            if(_command STREQUAL "target_link_libraries") # This if could be replaced with a command->function mapping
+                cmakejson_run_func_over_parsed_range(CMakeJSON_PARSE_TARGET_${parse_command}_${_access} cmakejson_gather_json_array_${_command}_as_list _params)
+            else()
+                cmakejson_run_func_over_parsed_range(CMakeJSON_PARSE_TARGET_${parse_command}_${_access} cmakejson_gather_json_array_as_list _params)
+            endif()
             if(_params)
                 cmake_language(EVAL CODE "set(_params ${_params})") # Evaluate variables stored in parsed variables.
-                #list(LENGTH _params _length)
-                #message(STATUS "LENGTH:${_length}") 
-                #if(_length GREATER 1)
-                #    list(GET _params 1 _elem)
-                #    message(STATUS "ELEMENT:${_elem}") 
-                #endif()
                 cmakejson_message_if(CMakeJSON_DEBUG_TARGET "Target command: ${_command}(${target_name} ${_access} ${_params})")
                 cmake_language(CALL ${_command} ${target_name} ${_access} ${_params})
             endif()
@@ -73,23 +83,104 @@ function(cmakejson_add_target _input _filename)
         unset(parse_command)
     endforeach()
 
+    set(ide_sources ${target_sources})
+    foreach(_command target_sources) # extra case for target_sources and add_dependencies
+        string(TOUPPER "${_command}" parse_command)
+        foreach(_access PUBLIC PRIVATE INTERFACE)
+            set(_params)
+            cmakejson_message_if(CMakeJSON_DEBUG_TARGET_VERBOSE "Checking: CMakeJSON_PARSE_TARGET_${parse_command}_${_access}")
+            cmakejson_run_func_over_parsed_range(CMakeJSON_PARSE_TARGET_${parse_command}_${_access} cmakejson_gather_json_array_as_list _params)
+            if(_params)
+                cmake_language(EVAL CODE "set(_params ${_params})") # Evaluate variables stored in parsed variables.
+                cmakejson_message_if(CMakeJSON_DEBUG_TARGET "Target command: ${_command}(${target_name} ${_access} ${_params})")
+                cmake_language(CALL ${_command} ${target_name} ${_access} ${_params})
+                if(_command STREQUAL "target_sources")
+                    list(APPEND ide_sources ${_params})
+                endif()
+            endif()
+            unset(_params)
+        endforeach()
+        unset(parse_command)
+    endforeach()
+
+    foreach(_command add_dependencies) # extra case for target_sources and add_dependencies
+        string(TOUPPER "${_command}" parse_command)
+        set(_params)
+        cmakejson_message_if(CMakeJSON_DEBUG_TARGET_VERBOSE "Checking: CMakeJSON_PARSE_TARGET_${parse_command}_${_access}")
+        cmakejson_run_func_over_parsed_range(CMakeJSON_PARSE_TARGET_${parse_command}_${_access} cmakejson_gather_json_array_as_list _params)
+        if(_params)
+            cmake_language(EVAL CODE "set(_params ${_params})") # Evaluate variables stored in parsed variables.
+            cmakejson_message_if(CMakeJSON_DEBUG_TARGET "Target command: ${_command}(${target_name} ${_access} ${_params})")
+            cmake_language(CALL ${_command} ${target_name} ${_access} ${_params})
+        endif()
+        unset(_params)
+        unset(parse_command)
+    endforeach()
+
+    set(IS_OBJECT_LIBRARY FALSE)
+    if(CMakeJSON_PARSE_TARGET_PARAMETERS MATCHES "OBJECT")
+        set(IS_OBJECT_LIBRARY TRUE)
+    endif()
+    set(IS_INTERFACE_LIBRARY FALSE)
+    if(CMakeJSON_PARSE_TARGET_PARAMETERS MATCHES "INTERFACE")
+        set(IS_INTERFACE_LIBRARY TRUE)
+    endif()
+
     # TODO Some extra CMakeJSON setup
+    if(IS_INTERFACE_LIBRARY AND NOT CMakeJSON_PARSE_TARGET_NO_IDE_INTERFACE_TARGET)
+        # Create a target for INTERFACE targets so the sources are visible in an IDE
+        add_custom_target(${target_name}_IDE DEPENDS ${target_name} SOURCES ${ide_sources})
+    endif()
+
+
+    if(NOT DEFINED CMakeJSON_PARSE_TARGET_EXPORT)
+        set(CMakeJSON_PARSE_TARGET_EXPORT ON)
+        if(CMakeJSON_PARSE_TARGET_TARGET_TYPE STREQUAL "custom_target")
+            set(CMakeJSON_PARSE_TARGET_EXPORT OFF)
+        elseif(IS_OBJECT_LIBRARY)
+            set(CMakeJSON_PARSE_TARGET_EXPORT OFF)
+        endif()
+    endif()
+    set(export_options)
+    if(CMakeJSON_PARSE_TARGET_EXPORT)
+        cmakejson_set_project_property(APPEND_OPTION APPEND PROPERTY EXPORTED_TARGETS ${target_name})
+        cmakejson_get_project_property(PROPERTY EXPORT_NAME)
+        set(export_options EXPORT "${EXPORT_NAME}")
+    endif()
+
+    if(NOT CMakeJSON_PARSE_TARGET_INSTALL_PARAMETERS)
+        cmakejson_get_project_property(PROPERTY PUBLIC_HEADER_INSTALL_DESTINATION)
+        # TODO: Make this better customizable from the json instead of overwriting everything. 
+        install(TARGETS ${target_name}
+                ${export_options}
+                RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR} COMPONENT Runtime
+                LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR} COMPONENT Development
+                ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR} COMPONENT Development
+                PUBLIC_HEADER DESTINATION ${PUBLIC_HEADER_INSTALL_DESTINATION} COMPONENT Development)
+    else()
+        cmake_language(EVAL CODE "set(_params ${CMakeJSON_PARSE_TARGET_INSTALL_PARAMETERS})") # Evaluate variables stored in parsed variables.
+        install(TARGETS ${target_name}
+                ${export_options}
+                ${_params})
+        unset(_params)
+    endif()
+
+    cmakejson_get_project_property(PROPERTY PACKAGE_NAME)
+    set_property(TARGET ${target_name} APPEND PROPERTY CMakeJSON_PACKAGE_NAME ${PACKAGE_NAME})
+    set_property(TARGET ${target_name} APPEND PROPERTY EXPORT_PROPERTIES CMakeJSON_PACKAGE_NAME)
 
     list(POP_BACK CMAKE_MESSAGE_CONTEXT)
 endfunction()
 
 function(cmakejson_target_file _file)
     file(TO_CMAKE_PATH "${_file}" _file)
-    if(IS_ABSOLUTE "${_file}")
-        set(file "${_file}")
-    else()
-        if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/${_file}")
-            set(file "${CMAKE_CURRENT_SOURCE_DIR}/${_file}")
-        else()
-            message(FATAL_ERROR "File '${_file}' does not exists!\nNeither is it absolute nor does it exists in the directory '${CMAKE_CURRENT_SOURCE_DIR}'!")
-        endif()
+    get_filename_component(file "${_file}" ABSOLUTE)
+    if(NOT EXISTS "${file}")
+        message(FATAL_ERROR "File '${_file}' does not exists!")
     endif()
+    message(${CMakeJSON_MSG_VERBOSE_TYPE} "Creating target from file: '${_file}'")
     get_filename_component(_filename "${_file}" NAME_WE)
-    file(READ "${_file}" _contents)
+    file(READ "${file}" _contents)
     cmakejson_add_target("${_contents}" "${_filename}")
+    set_property(DIRECTORY APPEND PROPERTY CMAKE_CONFIGURE_DEPENDS "${file}")
 endfunction()
